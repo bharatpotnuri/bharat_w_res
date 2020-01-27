7171a0e7d93da946418cbc2421902c354baa4ea9
Potnuri Bharat Teja <bharat@chelsio.com>
2020-01-27 18:17:47 +0530

    script to print the unique qids from huge logs.

---
+++ debug_scripts/HW/print_unique_qid.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+j=5
+for ((i=1; i<=4; i++, j++)); do
+{
+	cat mbw_cli_$i.txt |grep BHar| awk {'print $9'} > f_cli_$i.txt
+		awk -F',' '{print $0}' f_cli_$i.txt | sort | uniq > f$j.txt
+		echo -------------
+		echo mbw_cli_$i.txt                   
+		echo -------------
+		cat f$j.txt
+		total_cli=`cat "f_cli_""$i".txt | wc -l`
+		unique_cli=`cat f"$j".txt | wc -l`
+		echo "Total unique cq->cqid         = $unique_cli    values"
+		echo "Total generated cq->cqid      = $total_cli   values"
+}
+done
+
+
+
+
+
+
