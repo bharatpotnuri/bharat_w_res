02b7f3204cfa5ae15ba12e091b14ed830982e930
Chelsio Cudbg <chelsiocudbg@users.noreply.github.com>
2020-08-18 16:28:49 +0530

    couple of old changes

--- test_code/test_print_int.c
+++ test_code/test_print_int.c
@@ -6,12 +6,15 @@ int main ()
 {
 	unsigned long long int a;
 	long long int b;
+	unsigned int c = 0;
 	u_int64_t page_mask;
 	a = 112574691000000000;
 	b = a;
 	printf("a %llu  b %lld\n", a, b);
 	page_mask = ~(0x40000000 - 1);
 	printf("page mask %llx\n", page_mask);
+
+	printf("size of uint %d\n", sizeof(unsigned int));
 
 	return 0;
 }
