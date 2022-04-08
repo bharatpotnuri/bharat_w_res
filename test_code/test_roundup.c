63e4d0bf7c9732ff0fae3c52ca943790cdcb29b1
Potnuri Bharat Teja <bharat@chelsio.com>
2022-04-08 19:31:57 +0530

    test

--- test_code/test_roundup.c
+++ test_code/test_roundup.c
@@ -5,14 +5,15 @@ int main ()
 {
 #define roundup(x, y) (					\
 {							\
-	typeof(y) __y = y;				\
-	(((x) + (__y - 1)) / __y) * __y;		\
+	/*typeof(y) __y = y;		*/		\
+	/*(((x) + (__y - 1)) / __y) * __y;	*/	\
+	(((x) + (y - 1)) / y) * y;		\
 }							\
 )
 	int i, r = 0;
 
-	for (r; r<=256; r++) {
-		i = roundup(r, 32);
-		printf("roundup %d\n", i);
+	for (r; r<=50; r++) {
+		i = roundup(r, 16);
+		printf("roundup of %d = %d\n", r, i);
 	}
 }
